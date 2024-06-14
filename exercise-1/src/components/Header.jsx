71154481634a8a52f658d8f85856ccf9e9cbcd4f
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-06-14 15:54:21 +0700

    Create Header component

---
+++ exercise-1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header({ batchName }) {
+    return (
+        <header id="header">
+            <img src={pnLogo} alt="PN Logo" />
+            <h1>Students results for {batchName}</h1>
+        </header>
+    )
+}
+export default Header;
